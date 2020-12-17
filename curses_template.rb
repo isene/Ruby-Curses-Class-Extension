0443f1b3928255f3e2041716a9ae3a42d44b185e
Geir Isene <g@isene.com>
2020-12-17 20:46:57 +0100

    Added my Curses template

---
+++ curses_template.rb
@@ -0,0 +1,152 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# This is a basic template for my Curses applications. Feel free to use.
+
+require 'io/console'
+require 'curses'
+include  Curses
+
+Curses.init_screen
+Curses.start_color
+Curses.curs_set(0)
+Curses.noecho
+Curses.cbreak
+Curses.stdscr.keypad = true
+
+class Curses::Window # CLASS EXTENSION 
+  attr_accessor :fg, :bg, :attr, :text, :update, :pager, :pager_more, :pager_cmd, :locate, :nohistory 
+  # General extensions (see https://github.com/isene/Ruby-Curses-Class-Extension)
+  def clr
+    self.setpos(0, 0)
+    self.maxy.times {self.deleteln()}
+    self.refresh
+    self.setpos(0, 0)
+  end
+  def fill # Fill window with color as set by :bg
+    self.setpos(0, 0)
+    self.bg = 0 if self.bg   == nil
+    self.fg = 255 if self.fg == nil
+    init_pair(self.fg, self.fg, self.bg)
+    blank = " " * self.maxx
+    self.maxy.times {self.attron(color_pair(self.fg)) {self << blank}}
+    self.refresh
+    self.setpos(0, 0)
+  end
+  def write # Write context of :text to window with attributes :attr
+    self.bg   = 0 if self.bg   == nil
+    self.fg   = 255 if self.fg == nil
+    init_pair(self.fg, self.fg, self.bg)
+    self.attr = 0 if self.attr == nil
+    self.attron(color_pair(self.fg) | self.attr) { self << self.text }
+    self.refresh
+    self.text = ""
+  end
+  def p(fg, bg, attr, text)
+    init_pair(fg, fg, bg)
+    self.attron(color_pair(fg) | attr) { self << text }
+    self.refresh
+  end
+end
+
+def getchr # PROCESS KEY PRESSES
+  c = STDIN.getch(min: 0, time: 3)
+  case c
+  when "\e"    # ANSI escape sequences
+    case $stdin.getc
+    when '['   # CSI
+      case $stdin.getc
+      when 'A' then chr = "UP"
+      when 'B' then chr = "DOWN"
+      when 'C' then chr = "RIGHT"
+      when 'D' then chr = "LEFT"
+      when 'Z' then chr = "S-TAB"
+      when '2' then chr = "INS"    ; STDIN.getc
+      when '3' then chr = "DEL"    ; STDIN.getc
+      when '5' then chr = "PgUP"   ; STDIN.getc
+      when '6' then chr = "PgDOWN" ; STDIN.getc
+      when '7' then chr = "HOME"   ; STDIN.getc
+      when '8' then chr = "END"    ; STDIN.getc
+      end
+    end
+  when "", "" then chr = "BACK"
+  when "" then chr = "WBACK"
+  when "" then chr = "LDEL"
+  when "" then chr = "C-T"
+  when "\r" then chr = "ENTER"
+  when "\t" then chr = "TAB"
+  when /./  then chr = c
+  end
+  return chr
+end
+
+def main_getkey # GET KEY FROM USER
+  chr = getchr
+  case chr
+  when '?' # Show helptext in right window 
+    
+  when 'UP'
+    @index = @index <= @min_index ? @max_index : @index - 1
+  when 'DOWN'
+    @index = @index >= @max_index ? @min_index : @index + 1
+  when 'PgUP'
+    @index -= @w_l.maxy - 2
+    @index = @min_index if @index < @min_index
+  when 'PgDOWN'
+    @index += @w_l.maxy - 2
+    @index = @max_index if @index > @max_index
+  when 'HOME'
+    @index = @min_index
+  when 'END'
+    @index = @max_index
+  when 'l'
+    # ...etc
+  when 'q' # Exit 
+    exit 0
+  end
+end
+
+# TOP WINDOW FUNCTIONS 
+
+# BOTTOM WINDOW FUNCTIONS 
+
+# LEFT WINDOW FUNCTIONS
+
+# RIGHT WINDOW FUNCTIONS 
+
+
+# MAIN PROGRAM 
+loop do # OUTER LOOP - (catching refreshes via 'r')
+  @break = false # Initialize @break variable (set if user hits 'r')
+  begin # Create the four windows/panels 
+    maxx = Curses.cols
+    exit if maxx < @w_l_width
+    maxy = Curses.lines
+    # Curses::Window.new(h,w,y,x)
+    @w_t = Curses::Window.new(1, maxx, 0, 0)
+    @w_b = Curses::Window.new(1, maxx, maxy - 1, 0)
+    @w_l = Curses::Window.new(maxy - 2, @w_l_width - 1, 1, 0)
+    @w_u = Curses::Window.new(maxy/2 - 1, maxx - @w_l_width, 1, @w_l_width)
+    @w_d = Curses::Window.new(maxy/2, maxx - @w_l_width, maxy/2, @w_l_width)
+    loop do # INNER, CORE LOOP 
+      @min_index = 0
+      @max_index = @weather.size - 1
+      # Top window (info line) 
+      
+      # Bottom window (command line)
+
+      # Left window
+
+      # Right window
+
+      main_getkey        # Get key from user 
+
+      break if @break    # Break to outer loop, redrawing windows, if user hit 'r'
+      break if Curses.cols != maxx or Curses.lines != maxy # break on terminal resize 
+    end
+  ensure # On exit: close curses, clear terminal 
+    close_screen
+  end
+end
+
+# vim: set sw=2 sts=2 et fdm=syntax fdn=2 fcs=fold\:\ :
